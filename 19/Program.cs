5f369625669cd9cd825d6ca1f465d9a6d66b7274
Gulperiza <gulperiza96@gmail.com>
2023-02-10 21:51:55 +0500

    Палиндром

---
+++ 19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a/1000%10 == a/10%10) 
+{
+
+}
+else
+{
+   System.Console.WriteLine("Число не является палиндромом"); 
+}
+if (a/10000%10 == a/100000*10+a%10)
+{
+System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+System.Console.WriteLine("Число не является палиндромом");
+}
+
+  
